594604ad0f7988fd08f29158f2d12d94cf2e63c3
josephhuxley <joeykhuang@gmail.com>
2021-02-07 14:05:00 +0800

    added

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
-
-alias cls='clear' # Good 'ol Clear Screen command
+alias cls='clear'
+alias myip='curl http://ipecho.net/plain; echo'
+alias server='python2 -m SimpleHTTPServer 8000'
