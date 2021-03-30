134a29cedf9caedbee26e014059f67a3c3088b9b
josephhuxley <joeykhuang@gmail.com>
2021-03-30 15:31:53 +0800

    new zsh options

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias reload!='. ~/.zshrc'
 alias cls='clear'
 alias myip='curl http://ipecho.net/plain; echo'
 alias server='python2 -m SimpleHTTPServer 8000'
+alias pip='/Users/joeykhuang/anaconda3/bin/pip3'
+alias pku="cd ~/Desktop/PKU/"
